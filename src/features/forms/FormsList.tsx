4b5ab2ce0572d5b9c80c63a2780788516541319e
Amine TABOU <amin.tabou@gmail.com>
2026-03-26 16:13:37 +0100

    fix: adjust body styles for consistent layout and update FormsList component to use fullWidth prop

--- src/features/forms/FormsList.tsx
+++ src/features/forms/FormsList.tsx
@@ -64,7 +64,7 @@ export const FormsList = ({
         onClear={() => onSearchChange("")}
         label="Rechercher"
         showResetButton={!!searchQuery}
-        style={{ width: "100%" }}
+        fullWidth
       />
       {!hasResults ? (
         <EmptyCard message="Aucun formulaire trouvé" />
